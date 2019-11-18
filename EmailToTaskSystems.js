17bef20216c91fbabfa63340bb687c06a9ad6a1c
Daniel Wiesenthal <danwiesenthal@users.noreply.github.com>
2019-11-18 12:09:42 -0800

    catch newline edge case

--- EmailToTaskSystems.js
+++ EmailToTaskSystems.js
@@ -2,7 +2,7 @@
 // Add your email address(es) and label(s):
 
 // paste your task system ingest email here:
-var taskEmail = 'YOUR_TASK_SYSTEM_EMAIL_HERE';
+var taskEmail = 'YOUR_TASK_SYSTEMS_EMAIL_HERE';
 // update this label if you don't like the default:
 var taskProcessedLabelString = 'Script_Processed/Tasks';
 // update this prefix if you don't like the default:
@@ -13,7 +13,7 @@ var taskPrefix = '[Task] '
 
 
 
-// No need to read below this line unless you want to!  
+// No need to read below this line unless you want to!
 // If you've put your email and label information in above, you're good to go :)
 
 
@@ -28,46 +28,54 @@ var UNABLE_TO_PARSE_MESSAGE_ID = 'Unable to parse universal Message-Id';
 
 function getUniversalMessageId(message) {
   Logger.log('Getting universal message id for message %s', message);
+
+  var googleId = message.getId();
+  Logger.log('Google internal message id (different from header): %s', googleId);
+
   var rawContent = message.getRawContent();
-  Logger.log('raw_content: %s', rawContent);
+  // Logger.log('raw_content: %s', rawContent);
+
   var rawContentLines = rawContent.split('\n');
-  Logger.log('raw_content_lines: %s', rawContentLines);
-  Logger.log('Raw content has %s lines', rawContentLines.length);
+  // Logger.log('raw_content_lines: %s', rawContentLines);
+
   for (var i = 0; i < rawContentLines.length; i++){
+
     var line = rawContentLines[i];
-    Logger.log('line: %s', line);
-    Logger.log('type of line: %s', typeof(line));
-    Logger.log('substring: %s', line.substring(0, 10));
+
     if (line.substring(0, 10).toLowerCase() === HEADER_FIELD_TO_USE_AS_MESSAGE_ID) {
+      Logger.log('Found the message id header field');
+
       var messageIdComponents = line.split(' ');
-      Logger.log('message_id_components: %s', messageIdComponents);
+      Logger.log('messageIdComponents: %s', messageIdComponents);
 
       // at this point, either messageIdComponents[1] holds the Message-ID value (with brackets),
-      // or the sender (eg outlook which behaves differently) has a newline following the Message-ID key and the actual value
-      // is on the next line
-      if (typeof messageIdComponents[1] === "undefined") {
+      // or the sender (eg outlook ugh) has a newline following the Message-ID key and the actual value
+      // is on the next line (with this line being either undefined or a newline)
+      if ((typeof messageIdComponents[1] === "undefined") || (messageIdComponents[1].length <= 1)) {
         Logger.log("Message-ID header line appears to be split into two lines");
         Logger.log("Next line: " + rawContentLines[i + 1]);
         var idWithAligatorBrackets = rawContentLines[i + 1];
       } else {
         var idWithAligatorBrackets = messageIdComponents[1];
       };
-      Logger.log('id_with_aligagtor_brackets: %s', idWithAligatorBrackets);      
-      
-      var messageId = idWithAligatorBrackets.split('<')[1].split('>')[0];      
+      Logger.log('id_with_aligagtor_brackets: %s', idWithAligatorBrackets);
+
+      var messageId = idWithAligatorBrackets.split('<')[1].split('>')[0];
 
       Logger.log('Universal Message-ID parsed from header: %s', messageId);
       return messageId;
     };
   };
+
+  // If we can't parse out a message id but don't care to debug it, we can just return our failure
   return UNABLE_TO_PARSE_MESSAGE_ID;
 };
 
 function SendNewMessageWithCustomBody(originalMessage) {
   // Create subject and body of new message to send
   var prefixedSubject = taskPrefix + originalMessage.getSubject();
-  var body = originalMessage.getPlainBody() ? originalMessage.getPlainBody() : originalMessage.getBody();  
-  
+  var body = originalMessage.getPlainBody() ? originalMessage.getPlainBody() : originalMessage.getBody();
+
   // Try to get the universal message id
   var messageId = getUniversalMessageId(originalMessage);
   if (messageId === UNABLE_TO_PARSE_MESSAGE_ID) {
@@ -75,10 +83,10 @@ function SendNewMessageWithCustomBody(originalMessage) {
   } else {
     var messageLink = 'message:<' + messageId + '>';
   };
-  
+
   // Modify message body to include message link
   var bodyWithLink = 'View Message in Mail: '+ messageLink + '\n\n' + body;
-  
+
   // Send out the email to all emails listed in email_label_pairs
   Logger.log('emailLabelPairs in send func: %s', emailLabelPairs);
   emailLabelPairs.forEach(function(emailLabelPair) {
@@ -91,7 +99,7 @@ function markImportantAddLabelsUnstarMessagesArchiveThread(thread) {
   Logger.log('Marking thread important');
   // Mark the thread important
   thread.markImportant();
-  
+
   Logger.log('Adding processed labels');
   // Add each processed label to the thread
   emailLabelPairs.forEach(function(emailLabelPair) {
@@ -99,24 +107,24 @@ function markImportantAddLabelsUnstarMessagesArchiveThread(thread) {
     var actualLabel = GmailApp.getUserLabelByName(emailLabelPair[1]);
     thread.addLabel(actualLabel);
   });
-  
+
   Logger.log('Unstarring messages in thread');
   // Unstar each message in the thread
   var thread_messages = thread.getMessages();
   thread_messages.forEach(function(message) {
     message.unstar();
   });
-  
+
   Logger.log('Archiving the thread');
   // Archive the thread
   GmailApp.moveThreadToArchive(thread);
-  
+
   Logger.log('Leaving markImportant... function');
 };
 
 function main() {
   Logger.log('Beginning main');
-  
+
   // Go through the inbox threads:
   var inbox_threads = GmailApp.getInboxThreads();
   inbox_threads.forEach(function(thread) {
@@ -130,4 +138,3 @@ function main() {
     });
   });
 };
-
